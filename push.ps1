d515b4b122fd522c81013d0711a1fdbf066949a7
Nathan Etinzon <nathanetinzon@hotmail.fr>
2023-11-02 17:24:43 +0100

    Added my project

--- push.ps1
+++ push.ps1
@@ -1,5 +1,5 @@
 $Commit_name = Read-Host -Prompt 'Entrez le nom du commit'
-git add .
-git commit -m '$Commit_name'
+git add .\.gitignore .\docs\ .\mkdocs.yml
+git commit -m $Commit_name
 git push CEFIM master
 mkdocs gh-deploy -r CEFIM
